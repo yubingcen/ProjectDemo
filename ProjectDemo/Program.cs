99969fc28dcc825cca64c9e188d5284c7958137b
Bingcen Yu <644268318@qq.com>
2017-05-23 10:05:25 +0800

    新增启动页

--- ProjectDemo/Program.cs
+++ ProjectDemo/Program.cs
@@ -16,6 +16,23 @@ namespace ProjectDemo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // 启动  
+            SplashScreen.ShowSplashScreen();
+
+            // 进行自己的操作：加载组件，加载文件等等  
+            // 示例代码为休眠一会  
+            System.Threading.Thread.Sleep(3000);
+
+            // 关闭  
+            if (SplashScreen.Instance != null)
+            {
+                SplashScreen.Instance.BeginInvoke(new MethodInvoker(SplashScreen.Instance.Dispose));
+                SplashScreen.Instance = null;
+            }
             Application.Run(new Form1());
         }
     }
